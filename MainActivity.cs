a4d28132a25afdfe53d3a2bdab4736776118abbd
unknown-user <unknown-user@fe80::d0a6:adab:ae11:248b%3>
2014-10-14 20:58:08 +0200

    Importación inicial del módulo ComunicacionEntreFormularios

---
+++ MainActivity.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace ComunicacionEntreFormularios
+{
+	[Activity (Label = "ComunicacionEntreFormularios", MainLauncher = true, Icon = "@drawable/icon")]
+	public class MainActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			// Set our view from the "main" layout resource
+			SetContentView (Resource.Layout.Main);
+
+			Button btnLlamar = FindViewById<Button> (Resource.Id.btnLlamaDatos);
+
+			btnLlamar.Click += (object sender, EventArgs e) => {
+				var miActivity = new Intent(this,typeof(RecogeDatosActivity));
+
+				EditText TxtNombre = FindViewById<EditText>(Resource.Id.txtNombre);
+				miActivity.PutExtra("nombre", TxtNombre.Text);
+				StartActivityForResult(miActivity,1);
+			};
+		}
+
+		protected override void OnActivityResult(int requestCode, Result resultCode, Intent data){
+			base.OnActivityResult(requestCode, resultCode, data);
+
+			string mensaje = "";
+
+			switch (requestCode) {
+			case 1:
+				string nombre = data.GetStringExtra ("nombre");
+				string apellidos = data.GetStringExtra ("apellidos");
+				string edad = data.GetStringExtra ("edad");
+				mensaje= "Bienvenido " + nombre + " " + apellidos + " ya te han caido " + edad + " anos.";
+				break;
+			default:
+				mensaje = "No se ha llamado desde este Activity";
+				break;
+			}
+
+			// Prueba para ver si sube la version
+			Toast.MakeText (this, mensaje, ToastLength.Long).Show();
+		}
+	}
+}
+
+
---
+++ RecogeDatosActivity.cs
@@ -0,0 +1,46 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+
+namespace ComunicacionEntreFormularios
+{
+	[Activity (Label = "RecogeDatosActivity")]			
+	public class RecogeDatosActivity : Activity
+	{
+		protected override void OnCreate (Bundle bundle)
+		{
+			base.OnCreate (bundle);
+
+			SetContentView (Resource.Layout.RecogeDatos);
+
+			TextView txtNombre = FindViewById<TextView> (Resource.Id.txtNombreDatos);
+			txtNombre.Text = Intent.GetStringExtra ("nombre");
+
+			Button btnAceptar = FindViewById<Button> (Resource.Id.btnAceptar);
+
+			btnAceptar.Click += (sender, e) => {
+				Intent miActivity = new Intent(this,typeof(MainActivity));
+
+				miActivity.PutExtra("nombre",txtNombre.Text);
+
+				EditText txtApellidos = FindViewById<EditText> (Resource.Id.txtApellidos);
+				miActivity.PutExtra("apellidos",txtApellidos.Text);
+
+				EditText txtEdad = FindViewById<EditText> (Resource.Id.txtEdad);
+				miActivity.PutExtra("nombre",txtEdad.Text);
+
+				SetResult (Result.Ok, miActivity);
+				Finish();
+			};
+		}
+	}
+}
